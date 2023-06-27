4ac72bbac98dc6db9b0472135c0e1180ea81c355
bharathh2004 <136560082+bharathh2004@users.noreply.github.com>
2023-06-27 09:43:14 +0530

    Add files via upload

--- upperandlower.py
+++ upperandlower.py
@@ -1,11 +1,13 @@
+def string(x):
+    upper_case = 0
+    lower_case = 0
+    for i in x:
+        if i.isupper():
+            upper_case+=1
+        elif i.islower():
+            lower_case+=1
+    print("No of upper case:",upper_case)
+    print("No of lower case:",lower_case)
 
-x = 'The quick Brow Fox'
-upper_case = 0
-lower_case = 0
-for i in x:
-    if i.isupper():
-        upper_case +=1
-    elif i.islower():
-        lower_case +=1
-print("No of upper case:",upper_case)
-print("No of lower case:",lower_case)
+string('The quick Brow Fox')
+
